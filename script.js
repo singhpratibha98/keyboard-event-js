100254042ff2ce9e0b7924e4955154691c9fb3e1
pratibha singh <ps6977616@gmail.com>
2023-10-11 22:30:45 +0530

    done

---
+++ script.js
@@ -0,0 +1,20 @@
+let keyBox = document.querySelector(".keybox");
+
+// const audio = new Audio("./mixkit-hard-single-key-press-in-a-laptop-2542.wav");
+document.body.addEventListener("keydown", function(e){
+    let key = e.key;
+    if(e.shiftKey && e.keyCode!=16){
+        key = `Shift+${e.key}`
+    }
+    if(e.ctrlKey&& e.keyCode!=17){
+        key = `Ctrl+${e.key}`
+    }
+
+    let keyCode = e.keyCode;
+
+    keyBox.innerHTML = `
+    <h1 class = "key">You Pressed: <span>${key}</span></h1>
+    <div class = "code">${keyCode}</div>`;
+    
+    // audio.play();
+});
